3507ff60df70293c25c612db48039c058ea5f40b
Johan Alejandro Castrillon <alejocastrillon13@gmail.com>
2022-11-24 21:37:35 -0500

    agregado de main para correr como script

--- Main.py
+++ Main.py
@@ -1,30 +1,33 @@
 import Utils.Charge as Charge
 import Utils.Graphics as gf
 import Utils.procesingData as procesingData
-confirm = False
-while confirm == False:
-    FileName = input(f'Ingresar el nombre del archivo ha utilizar o presione enter para salir:')
-    if FileName == '':
-        break
-    else:
-        data,confirm = Charge.cargaArchivo(FileName)
 
-confirm = False
-menu, cols = procesingData.colNames(data)
-while confirm == False:
-    use = input(menu)
-    if use == "":
-        break
-    elif use not in cols:
-        print('Opcion no valida')
-    else:
-        var =procesingData.separate(data,use)
-        unique,count = procesingData.countValues(var)
-        gf.Grafica(unique,count)
+def run():
+    confirm = False
+    while confirm == False:
+        FileName = input(f'Ingresar el nombre del archivo ha utilizar o presione enter para salir:')
+        if FileName == '':
+            break
+        else:
+            data,confirm = Charge.cargaArchivo(FileName)
+
+    confirm = False
+    menu, cols = procesingData.colNames(data)
+    while confirm == False:
+        use = input(menu)
+        if use == "":
+            break
+        elif use not in cols:
+            print('Opcion no valida')
+        else:
+            var =procesingData.separate(data,use)
+            unique,count = procesingData.countValues(var)
+            gf.Grafica(unique,count)
 
 
 
-
+if __name__ == "__main__":
+    run()
 
 
 
